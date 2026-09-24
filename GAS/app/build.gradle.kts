ddfe0dbf91c87c8e0e34a9b6ab1432c5934e84bc
aiagentid7-a11y <aiagentid7@gmail.com>
2026-09-24 10:15:39 +0700

    Keep source namespace while changing app ID

--- GAS/app/build.gradle.kts
+++ GAS/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-  namespace = "com.aiagentid7.payrollemployee"
+  namespace = "com.example"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
